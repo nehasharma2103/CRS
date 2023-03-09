e61ec3ed9f706056ddf6f70e1f1ea0fdb2bbf116
nehasharma2103 <nehapsharma4435@gmail.com>
2023-03-09 15:33:36 +0530

    Initial Commit

--- url.js
+++ url.js
@@ -2,4 +2,4 @@
 //module.exports = "mongodb://localhost:28017"
 // module.exports = "mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/?retryWrites=true&w=majority"
 // module.exports="mongodb+srv://admin:admin@mdb.ros9nlu.mongodb.net/?retryWrites=true&w=majority"
-mondule.exports="mongodb+srv://admin:<admin>@college.mxvuwtf.mongodb.net/?retryWrites=true&w=majority"
+module.exports="mongodb+srv://admin:admin@college.mxvuwtf.mongodb.net/?retryWrites=true&w=majority"
