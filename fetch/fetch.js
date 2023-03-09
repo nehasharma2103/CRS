ee15590367c21ace70f6307104c9b03500b6b7dd
nehasharma2103 <nehapsharma4435@gmail.com>
2023-03-09 14:24:09 +0530

    Initial Commit

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -55,7 +55,7 @@ router.post("/student", (req, res) => {
                 console.log('Error in connection ', err)
             else {
                 let db = conn.db('CRS')
-                db.collection('college').find().toArray((err, array) => {
+                db.collection('college1').find().toArray((err, array) => {
                     if (err)
                         console.log('Error while fetching data')
                     else {
@@ -67,15 +67,19 @@ router.post("/student", (req, res) => {
         })
     }),
 
-    router.post("/college", (req, res) => {
+    router.post("/college1", (req, res) => {
         //connect to mongodb
 
         //let stdper = parseFloat(req.body.per).toFixed(2)
-        let stdper = parseFloat((req.body.per))
+        // let stdper = parseFloat((req.body.per))
+        let minMerit = req.body.minMerit
+        let maxMerit = req.body.maxMerit
         let clgregion = req.body.Region
         let clgbranch = req.body.branch
         mcl.connect(url, (err, conn) => {
-            console.log("Percentage from user:- ", stdper)
+            // console.log("Percentage from user:- ", stdper)
+            console.log("Minimum Merit Score from User:- ", minMerit)
+            console.log("Maximum Merit Score from User:- ", maxMerit)
             console.log("Region from user:- ", clgregion)
             console.log("Branch from user", clgbranch)
             console.log('Inside Connect')
@@ -84,104 +88,109 @@ router.post("/student", (req, res) => {
             else {
                 let db = conn.db('CRS')
                 //db.collection('college').find({ Region: { $eq: clgregion } }).toArray((err, array) => {
-                db.collection('college').find().toArray((err, array) => {
+                db.collection('college1').find({ Region: clgregion }).toArray((err, array) => {
                     if (err)
                         console.log('Error while fetching data')
                     else {
                         console.log('Data sent')
 
-                        let uRegion = array.filter((e, i) => {
-                            return e.Region == clgregion
-                        })
-
-                        let branchArrayLess = []
-                        let branchArrayGret = []
-
-                        for (let i = 0; i < uRegion.length; i++) {
-                            let e = uRegion[i]
-                            for (let key in e) {
-                                if (key == clgbranch && e[key] < stdper)
-                                    branchArrayLess.push(e)
-                                if (key == clgbranch && e[key] >= stdper)
-                                    branchArrayGret.push(e)
-                            }
-                        }
-                        let sLess = branchArrayLess.sort((fv, nv) => {
-                            console.log("First value:- ", fv)
-                            return fv - nv
-                        })
-                        let sGret = branchArrayGret.sort((fv, nv) => {
-                            return nv - fv
-                        })
-
-                        console.log('Final Less array :-', branchArrayLess)
-                        console.log('Final Less Array Sorted:- ', sLess)
-
-                        console.log('Final Great array :-', branchArrayGret)
-                        console.log('Final Great Array Sorted:- ', sGret)
-
+                        const filterResult = array.filter(college1 => college1[clgbranch] !== undefined);
+                        const finalResult = array.filter(college1 => college1[clgbranch] >= minMerit && college1[clgbranch] <= maxMerit);
+                        finalResult.sort((a, b) => a[clgbranch] - b[clgbranch]);
+                        res.json(finalResult);
                         /*
-                        let sortedArray = sort_by_key(array, 'cutoff')
-                        console.log('Sored array by today :- ',sortedArray)
-
-                        let myarrg = sortedArray.filter((e, i) => {
-                            return e.cutoff >= stdper
-                        })
-                        let myarrl = sortedArray.filter((e, i) => {
-                            return e.cutoff < stdper
-                        })
-                        */
-                        let f1 = (sLess.slice(0, 3)).reverse()
-                        let f2 = (sGret.slice(-2)).reverse()
-                        let combined = f1.concat(f2)
-                        console.log("sorted array", combined)
-                        res.json(combined)
-
-                        /*
-                        let finalarr = []
-                        finalarr.push(myarrl.slice(-2))
-                        finalarr.push(myarrg.slice(0,2))
-                        console.log('Final Array :- ',finalarr)
-                        finalarr.flat(Infinity)
-                        res.json(finalarr)
+                                                let uRegion = array.filter((e, i) => {
+                                                    return e.Region == clgregion
+                                                })
                         
-                        console.log("Greater Array :- ",myarrg.slice(0,2))
-                        console.log("Lesser  Array :- ",myarrl.slice(-2))
+                                                let branchArrayLess = []
+                                                let branchArrayGret = []
                         
-                        console.log(array)
-                        function sort(e,i){
-                            return array.sort(function(a,b){
-                                debugger
-                                var x = a[i]; var y=b[i];
-                                return((x<y)<1:((x>y)>1:0));
-                            })
-                        }
+                                                for (let i = 0; i < uRegion.length; i++) {
+                                                    let e = uRegion[i]
+                                                    for (let key in e) {
+                                                        if (key == clgbranch && e[key] < stdper)
+                                                            branchArrayLess.push(e)
+                                                        if (key == clgbranch && e[key] >= stdper)
+                                                            branchArrayGret.push(e)
+                                                    }
+                                                }
+                                                let sLess = branchArrayLess.sort((fv, nv) => {
+                                                    console.log("First value:- ", fv)
+                                                    return fv - nv
+                                                })
+                                                let sGret = branchArrayGret.sort((fv, nv) => {
+                                                    return nv - fv
+                                                })
                         
-                        let maxobj = myarrg[0]
-                        let obj1 = myarrg.map((e, i) => {
-                            if (e.cutoff < maxobj.cutoff)
-                                maxobj = e
-                            return maxobj
-                        })
-                        console.log("Max obj = ", maxobj)
+                                                console.log('Final Less array :-', branchArrayLess)
+                                                console.log('Final Less Array Sorted:- ', sLess)
                         
-                        let myarrl = array.filter((e, i) => {
-                            return e.cutoff < stdper
-                        })
-                        let minobj = myarrl[0]
-                        obj1 = myarrl.map((e,i)=>{
-                            if(e.cutoff > minobj.cutoff)
-                                minobj = e
-                        })
-                        console.log("Min obj :- ",minobj)
-                        //res.json(myarr)
-                        let finalarr = []
-                        finalarr.push(minobj)
-                        finalarr.push(maxobj)
-                        res.json(finalarr)
-                        //res json array
-                        //let myarr= array.find()
-                        */
+                                                console.log('Final Great array :-', branchArrayGret)
+                                                console.log('Final Great Array Sorted:- ', sGret)
+                        
+                                                /*
+                                                let sortedArray = sort_by_key(array, 'cutoff')
+                                                console.log('Sored array by today :- ',sortedArray)
+                        
+                                                let myarrg = sortedArray.filter((e, i) => {
+                                                    return e.cutoff >= stdper
+                                                })
+                                                let myarrl = sortedArray.filter((e, i) => {
+                                                    return e.cutoff < stdper
+                                                })
+                                                
+                                                let f1 = (sLess.slice(0, 4)).reverse()
+                                                let f2 = (sGret.slice(-4)).reverse()
+                                                let combined = f1.concat(f2)
+                                                console.log("sorted array", combined)
+                                                res.json(combined)
+                        
+                                                /*
+                                                let finalarr = []
+                                                finalarr.push(myarrl.slice(-2))
+                                                finalarr.push(myarrg.slice(0,2))
+                                                console.log('Final Array :- ',finalarr)
+                                                finalarr.flat(Infinity)
+                                                res.json(finalarr)
+                                                
+                                                console.log("Greater Array :- ",myarrg.slice(0,2))
+                                                console.log("Lesser  Array :- ",myarrl.slice(-2))
+                                                
+                                                console.log(array)
+                                                function sort(e,i){
+                                                    return array.sort(function(a,b){
+                                                        debugger
+                                                        var x = a[i]; var y=b[i];
+                                                        return((x<y)<1:((x>y)>1:0));
+                                                    })
+                                                }
+                                                
+                                                let maxobj = myarrg[0]
+                                                let obj1 = myarrg.map((e, i) => {
+                                                    if (e.cutoff < maxobj.cutoff)
+                                                        maxobj = e
+                                                    return maxobj
+                                                })
+                                                console.log("Max obj = ", maxobj)
+                                                
+                                                let myarrl = array.filter((e, i) => {
+                                                    return e.cutoff < stdper
+                                                })
+                                                let minobj = myarrl[0]
+                                                obj1 = myarrl.map((e,i)=>{
+                                                    if(e.cutoff > minobj.cutoff)
+                                                        minobj = e
+                                                })
+                                                console.log("Min obj :- ",minobj)
+                                                //res.json(myarr)
+                                                let finalarr = []
+                                                finalarr.push(minobj)
+                                                finalarr.push(maxobj)
+                                                res.json(finalarr)
+                                                //res json array
+                                                //let myarr= array.find()
+                                                */
 
                     }
                 })
--- url.js
+++ url.js
@@ -1,4 +1,5 @@
 // module.exports="mongodb://127.0.0.1:27017"
 //module.exports = "mongodb://localhost:28017"
 // module.exports = "mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/?retryWrites=true&w=majority"
-module.exports="mongodb+srv://admin:admin@mdb.ros9nlu.mongodb.net/?retryWrites=true&w=majority"
+// module.exports="mongodb+srv://admin:admin@mdb.ros9nlu.mongodb.net/?retryWrites=true&w=majority"
+mondule.exports="mongodb+srv://admin:<admin>@college.mxvuwtf.mongodb.net/?retryWrites=true&w=majority"
